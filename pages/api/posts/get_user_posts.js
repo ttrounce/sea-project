0be420eb2ea31db902e038940f14b4f157bec649
Brian Evans <ebrian101@gmail.com>
2021-03-10 15:41:04 +0000

    Fixed bug with profile (getuserposts)
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- pages/api/posts/get_user_posts.js
+++ pages/api/posts/get_user_posts.js
@@ -34,7 +34,6 @@ export default async (req, res) => {
             const result = await db
                 .any(getUserStatement)
                 .then((result) => {
-                    pgp.end()
                     // If a result is found, send status 200 with relevant info in payload
                     res.status(200).json(result)
                 })
